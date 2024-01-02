4846be3f6e07058904aef10afafc9d5527dfc87d
Priya Jasmin <priya.r.jasmin@gmail.com>
2024-01-03 00:07:17 +0530

    Update arithmetic_op.sh

--- arithmetic_op.sh
+++ arithmetic_op.sh
@@ -1,25 +1,2 @@
-#5+50*3/20 + (19*2)/7
-
-solve()
-{
-    string=$1
-    for i in "${string[@]}";
-    do 
-        #solve $i
-        echo $i
-    done
-}
-
-
-read str
-arr=(${str// /})
-arr=(${arr//)/','})
-arr=(${arr//(/','})
-arr=(${arr//,/' '})
-
-for i in "${arr[@]}"
-do 
-    echo $i
-done
-
-solve ${arr[@]}
+read exp
+echo "scale = 3; $exp" | bc
